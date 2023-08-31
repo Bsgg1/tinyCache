9bcba9c7e46a0eb27500f45b3253af39a57941b0
ss qaq <2112536022@qq.com>
2023-08-31 11:50:53 +0800

    实现一致性hash

---
+++ consistenthash.go
@@ -0,0 +1,51 @@
+package tinyCache
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int //虚拟节点个数
+	keys     []int
+	hashMap  map[int]string // 虚拟节点与真实节点映射
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys) //把环上的hash排序
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	//因为是一个环状结构，大于所有的值时，就用最小值
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
